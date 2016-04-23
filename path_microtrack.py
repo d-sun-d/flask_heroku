d4d712b88fa637415e2ab5cd36f4058d2de5c501
Alexander Ryabchenko <d-sun-d@yandex-team.ru>
2016-04-24 01:37:07 +0300

    translate more text

--- path_microtrack.py
+++ path_microtrack.py
@@ -4,22 +4,22 @@ import libs.yandex_translate as ytr
 import libs.chartfirst_client as c1_client
 import pprint
 
-LOCAL_DB = {
+
+redis_db = {
     "tasks":{},
     "last_id":0
 }
 
-
 def path_microtrack_add():
     params = {key:rec for key, rec in request.args.items()}
-    id = LOCAL_DB["last_id"]
-    LOCAL_DB["last_id"] += 1
+    id = redis_db["last_id"]
+    redis_db["last_id"] += 1
     params["original_lang"] = ytr.detect_lang(params["text"])
     params["en_text"] = ytr.translate(params["text"], params["original_lang"]+"-en")
-    LOCAL_DB[id] = params
+    redis_db["tasks"][id] = params
 
     response = c1_client.ActionResponse()
-    new_task_text = ytr.translate("New task", params["original_lang"])
+    new_task_text = ytr.translate("Персонал оповещен, выполним в ближайшее время. \n\n Номер вашей заявки ", params["original_lang"])
     response.messages.append(new_task_text+" id:"+str(id))
 
     return make_response(jsonify(response.to_dict()), 200)
@@ -27,5 +27,5 @@ def path_microtrack_add():
 
 def path_get_db():
     response = c1_client.ActionResponse()
-    response.messages.append(pprint.pformat(LOCAL_DB))
+    response.messages.append(pprint.pformat(redis_db))
     return make_response(jsonify(response.to_dict()), 200)
