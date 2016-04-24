c11a5ca73d3a53b744a46c5abe668dc626824038
Alexander Ryabchenko <d-sun-d@yandex-team.ru>
2016-04-24 11:32:17 +0300

    try to add redis db

---
+++ libs/redis_cache.py
@@ -0,0 +1,44 @@
+import redis
+import simplejson
+
+REDIS_HOST = "ec2-54-83-34-248.compute-1.amazonaws.com"
+REDIS_PORT = 11209
+REDIS_PASS = 'pvc0s3mj8bjj9em016172mohj9'
+
+
+REDIS_DB_TEMPLATE = {
+    "tasks":{},
+    "last_id":0
+}
+
+class CacheService:
+    def __init__(self):
+        print "start redis init"
+        self.r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, password=REDIS_PASS, ssl=True)
+        print "end redis init"
+
+    def ping(self):
+        # Ping cache server
+        return self.r.ping()
+
+    def get(self, key):
+        # Get value by key from cache
+        return self.r.get(key)
+
+    def set(self, key, value):
+        # Save value to cache
+        return self.r.set(key, value)
+
+    def delete(self, key):
+        # Save value to cache
+        return self.r.delete(key)
+
+    def get_db(self):
+        result = self.get("REDES_DB")
+        if result is None:
+            return  REDIS_DB_TEMPLATE
+        return simplejson.loads(result)
+
+    def save_db(self, db):
+        db_string = simplejson.dumps(db)
+        self.set("REDES_DB", db_string)
--- path_microtrack.py
+++ path_microtrack.py
@@ -4,6 +4,7 @@ from flask import render_template, jsonify, make_response
 from flask import request
 import libs.yandex_translate as ytr
 import libs.chartfirst_client as c1_client
+from libs.redis_cache import CacheService
 import pprint
 
 
@@ -13,6 +14,10 @@ redis_db = {
 }
 
 def path_microtrack_add():
+    cache = CacheService()
+    print "start get db"
+    redis_db = cache.get_db()
+    print "end get db"
     params = {key:rec for key, rec in request.args.items()}
     id = redis_db["last_id"]
     redis_db["last_id"] += 1
@@ -24,6 +29,9 @@ def path_microtrack_add():
     new_task_text = ytr.translate("Персонал оповещен, выполним в ближайшее время.\n\n  Номер вашей заявки ", params["original_lang"])
     response.messages.append(new_task_text+" id:"+str(id))
 
+    print "start save"
+    cache.save_db(redis_db)
+    print "end save"
     return make_response(jsonify(response.to_dict()), 200)
 
 
